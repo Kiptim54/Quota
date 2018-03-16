b8a3d790a685c67e2a66938baedf35ed59f9e399
Brenda Kiptim <kiptim54@gmail.com>
2018-03-16 15:21:12 +0300

    add form layout

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote'
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+quotes=[];
+
+addQuote(username:HTMLInputElement, Quote:HTMLInputElement, author:HTMLInputElement):boolean{
+
+return false;
+}
+   console.log(`${Quote.value}`); 
+
+   ngOnInit(){
+
+}
+
